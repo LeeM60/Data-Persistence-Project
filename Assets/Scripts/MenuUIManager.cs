86a49e8b553ae28b52bed78cf366456856047876
LeeM60 <lee.madder@gmail.com>
2025-08-10 15:56:38 +0100

    UI Update

---
+++ Assets/Scripts/MenuUIManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIManager : MonoBehaviour
+{
+
+    public static TMP_Text playerName;
+
+    public void StartNew()
+    {
+        playerName = GetComponent<TMP_Text>();
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
